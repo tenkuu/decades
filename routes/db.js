d927afabce97b35560121f1a6d3b067a9bb885fd
Vlad Ryzhov <vryzhov@sfu.ca>
2021-07-11 23:15:11 -0700

    Ensure that we don't write to FS on GAE

--- routes/db.js
+++ routes/db.js
@@ -1,12 +1,17 @@
 const fse = require(`fs-extra`);
 
-const dbPath = process.env.NODE_ENV === "production" ? `routes/artworks_db_prod.json` : `routes/artworks_db_dev.json`;
+const dbPath = `routes/artworks_db.json`;
 
 const db = {
   GetArtworks: async function () {
     const dbExists = await fse.pathExists(dbPath);
     if (!dbExists) {
-      await fse.writeJSON(dbPath, { artworks: [] });
+      //On GAE we can't write to the file system
+      if (process.env.NODE_ENV !== "production") {
+        await fse.writeJSON(dbPath, { artworks: [] });
+      } else {
+        return { artworks: [] };
+      }
     }
 
     const data = await fse.readJson(dbPath);
@@ -18,64 +23,77 @@ const db = {
 
     //TODO: validate data before adding it.
 
-    data.artworks.push(artwork);
-    await fse.writeJSON(dbPath, data, { spaces: 4 });
+    //On GAE we can't write to the file system
+    if (process.env.NODE_ENV !== "production") {
+      data.artworks.push(artwork);
+      await fse.writeJSON(dbPath, data, { spaces: 4 });
+    }
+
     return data;
   },
 
   ResetWithRandomArtworks: async function (amount) {
     let data = await this.GetArtworks();
-    data.artworks = _generateRandomArtworks(amount)
-    await fse.writeJSON(dbPath, data, { spaces: 4 });
-    return data
-  }
+
+    //On GAE we can't write to the file system
+    if (process.env.NODE_ENV !== "production") {
+      data.artworks = _generateRandomArtworks(amount);
+      await fse.writeJSON(dbPath, data, { spaces: 4 });
+    }
+
+    return data;
+  },
 };
 
 ////// HELPERS
 
 const basicColors = [
-    [0, 0, 0],          //black
-    [255, 255, 255],    //white
-    [255, 0, 0],        //red
-    [0, 255, 0],        //green
-    [0, 0, 255],        //blue
-    [255, 255, 0],      //yellow
-    [255, 0, 255],      //pink
-    [0, 255, 255]       //cyan
-]
+  [0, 0, 0], //black
+  [255, 255, 255], //white
+  [255, 0, 0], //red
+  [0, 255, 0], //green
+  [0, 0, 255], //blue
+  [255, 255, 0], //yellow
+  [255, 0, 255], //pink
+  [0, 255, 255], //cyan
+];
 
 const _generateRandomColorInt = () => {
-    return [Math.floor(Math.random()*255), Math.floor(Math.random()*255), Math.floor(Math.random()*255)]
-}
+  return [
+    Math.floor(Math.random() * 255),
+    Math.floor(Math.random() * 255),
+    Math.floor(Math.random() * 255),
+  ];
+};
 
 const _getRandomLinearBitmap = (width, height) => {
-    const randomColorRGBInt = _generateRandomColorInt()
-    let bitmap = []
-    for (let row = 0; row < height; row++){
-        for (let col = 0; col<width; col++){
-            bitmap.push(randomColorRGBInt[0])
-            bitmap.push(randomColorRGBInt[1])
-            bitmap.push(randomColorRGBInt[2])
-        }
+  const randomColorRGBInt = _generateRandomColorInt();
+  let bitmap = [];
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      bitmap.push(randomColorRGBInt[0]);
+      bitmap.push(randomColorRGBInt[1]);
+      bitmap.push(randomColorRGBInt[2]);
     }
+  }
 
-    return bitmap
-}
+  return bitmap;
+};
 
 const _generateRandomArtworks = (amount) => {
-    let artworkObjects = []
-    for (let i = 0; i<amount; i++){
-        let artworkObject = {}
-        artworkObject.name = `artwork_name_${i}`
-        artworkObject.user = `user_name_${i}`
-        artworkObject.date = `01 ${i} 2020`
-        artworkObject.songId = `artist${i}/song${i}`
-        artworkObject.bitmap = _getRandomLinearBitmap(1, 1)
+  let artworkObjects = [];
+  for (let i = 0; i < amount; i++) {
+    let artworkObject = {};
+    artworkObject.name = `artwork_name_${i}`;
+    artworkObject.user = `user_name_${i}`;
+    artworkObject.date = `01 ${i} 2020`;
+    artworkObject.songId = `artist${i}/song${i}`;
+    artworkObject.bitmap = _getRandomLinearBitmap(1, 1);
 
-        artworkObjects.push(artworkObject)
-    }
+    artworkObjects.push(artworkObject);
+  }
 
-    return artworkObjects
-}
+  return artworkObjects;
+};
 
 module.exports = db;
