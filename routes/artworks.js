6feee4d2344becb5498b221f15b9b9ed7fb87876
Vlad Ryzhov <vladislav_ryzhov@sfu.ca>
2021-07-19 00:31:15 +0000

    Image upload

--- routes/artworks.js
+++ routes/artworks.js
@@ -1,16 +1,39 @@
 const express = require(`express`)
 const DB = require(`./src/db`)
+const uploader = require(`./src/upload_artwork`)
 
 const router = express.Router()
 
+//TODO: this logic is not present. Right now the client can simply download the bucket files.
+//Need to revise this and somehow supply the bitmaps from the server or make extra rules for who can download
+//bucket files.
+const _populateArtworksBitmaps = async (artworks) => {
+    for (let artwork of artworks.artworks){
+        const bitmap = await uploader.downloadArtwork(artwork)
+        artwork.bitmap = bitmap
+    }
+
+    return artworks
+}
+
+const _uploadArtworkAndFilterRequest = async (artwork) => {
+    //artwork.bitmap would have the bitmap
+    const artworkURL = await uploader.uploadArtwork(artwork)
+
+    //assign the link but unassign the bitmap
+    artwork.link = artworkURL
+    delete artwork.bitmap
+    return artwork
+}
+
 //DEBUG API
 router.get(`/debug/all`, async (req, res) => {
-    const allArtworks = await DB.GetArtworks(DB.access.ALL)
+    let allArtworks = await DB.GetArtworks(DB.access.ALL)
     res.json(allArtworks)
 })
 
 router.get(`/debug/:user`, async (req, res) => {
-    const allArtworks = await DB.GetArtworks(DB.access.ALL, req.params.user)
+    let allArtworks = await DB.GetArtworks(DB.access.ALL, req.params.user)
     res.json(allArtworks)
 })
 
@@ -18,32 +41,50 @@ router.get(`/debug/:user`, async (req, res) => {
 //Randomly generate artworks for testing
 router.post(`/debug/reset/:amount`, async (req, res) => {
     const artworks = await DB.ResetWithRandomArtworks(req.params.amount)
+    let defaultBitmap = []
+    for (let i = 0; i<640; i++){
+        for (let j = 0; j<640; j++){
+            defaultBitmap.push(255);
+        }
+    }
+
+    for (let artwork of artworks.artworks){
+        artwork.bitmap = defaultBitmap
+        artwork = await _uploadArtworkAndFilterRequest(artwork)
+        await DB.UpdateArtwork(artwork)
+    }
+
     res.json(artworks)
 })
 
 //PRODUCTION API
 router.get(`/uploaded`, async (req, res) => {
-    const uploadedArtworks = await DB.GetArtworks(DB.access.UPLOADED_ONLY)
+    let uploadedArtworks = await DB.GetArtworks(DB.access.UPLOADED_ONLY)
     res.json(uploadedArtworks)
 })
 
 router.get(`/local`, async (req, res) => {
     const fakeLoggedUser = 'user_name_6'
-    const localArtworks = await DB.GetArtworks(DB.access.ALL, fakeLoggedUser)
+    let localArtworks = await DB.GetArtworks(DB.access.ALL, fakeLoggedUser)
     res.json(localArtworks)
 })
 
 router.post(`/save`, async (req, res) => {
-    let updatedArtwork = await DB.UpdateArtwork(req.body)
+    const artwork = await _uploadArtworkAndFilterRequest(req.body)
+
+    //update DB
+    let updatedArtwork = await DB.UpdateArtwork(artwork)
     res.json(updatedArtwork)
 })
 
 router.post(`/upload`, async (req, res) => {
     //same as /save but we need to explicitly mark is as .public = true
     //SAVEs and UPLOADs
-    let artworkToUpload = req.body
-    artworkToUpload.public = true
-    let updatedArtwork = await DB.UpdateArtwork(artworkToUpload)
+    const artwork = await _uploadArtworkAndFilterRequest(req.body)
+    artwork.public = true
+    
+    //update DB
+    let updatedArtwork = await DB.UpdateArtwork(artwork)
     res.json(updatedArtwork)
 })
 
